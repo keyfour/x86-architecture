13814204cfe9ffa7ee47f5d3f624c6e13b148d50
Aleksandr Karpov <keyfour13@gmail.com>
2016-10-31 12:14:40 +0300

    Add script to run examples on android device

---
+++ run_on_android.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+#
+# Run executables on Android Device
+#
+
+##Change your adb path
+ADB=/usr/bin/adb
+
+[[ "$1" ]] || { echo "Usage: ./run_on_android.sh [executable file]" && exit 1; }
+
+EFILE=$1
+
+#Install program
+$ADB push $EFILE /data/local/tmp/$EFILE
+
+#Run in shell
+$ADB shell /data/local/tmp/$EFILE
